eecd8a5fedd4f0fe1425922ddc2dee40afd82ba4
danny <danny.nothdurft@sugarshape.de>
2023-07-04 19:15:46 +0200

    Bild über Cloud

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
     <main className="main--container--page">
       <div>
         <Image
-          src="/head.webp"
+          src="https://res.cloudinary.com/dca67w0ia/image/upload/v1688490841/head_sebyjd.webp"
           width={1100}
           height={385}
           style={{ height: "auto" }}
